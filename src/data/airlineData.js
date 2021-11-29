be45b3f8334137e407ad0f605b56b82924be6caa
MatthewClarkOSU <matthewclarkosu@gmail.com>
2021-11-29 11:25:13 -0500

    changed json data into JavaScript object

---
+++ src/data/airlineData.js
@@ -0,0 +1,218 @@
+export const airlineData = [
+    {
+    site: "https://www.airfrance.com",
+    code: "AF",
+    allliance: "ST",
+    phone: "1-800-237-2747",
+    name: "Air France",
+    defaultName: null,
+    __clazz: "com.r9.harmony.httpd.mobileapis.AirlineMobile",
+    __clazz: null,
+    logoURL: "/rimg/provider-logos/airlines/v/AF.png?crop=false&width=108&height=92&fallback=default1.png&_v=48aae6bb98d8c51316f5918500ed39b2"
+    },
+
+    {
+    site: "www.airindia.in/",
+    code: "AI",
+    allliance: "SA",
+    phone: "+91 22 27067001",
+    name: "Air India",
+    defaultName: null,
+    __clazz: "com.r9.harmony.httpd.mobileapis.AirlineMobile",
+    __clazz: null,
+    logoURL: "/rimg/provider-logos/airlines/v/AI.png?crop=false&width=108&height=92&fallback=default1.png&_v=18d6ba2bd1a6b89120361f37345483d6"
+    },
+
+    {
+    site: "https://www.alaskaair.com",
+    code: "AS",
+    allliance: "OW",
+    phone: "+1 800 252 75 22",
+    name: "Alaska Airlines",
+    defaultName: null,
+    __clazz: "com.r9.harmony.httpd.mobileapis.AirlineMobile",
+    __clazz: null,
+    logoURL: "/rimg/provider-logos/airlines/v/AS.png?crop=false&width=108&height=92&fallback=default2.png&_v=7e7c4110616a97db4d99676711cb7247"
+    },
+
+    {
+    site: "https://www.royalairmaroc.com/us-en",
+    code: "AT",
+    allliance: "OW",
+    phone: "+212 5 22 48 97 97",
+    name: "Royal Air Maroc",
+    defaultName: null,
+    __clazz: "com.r9.harmony.httpd.mobileapis.AirlineMobile",
+    __clazz: null,
+    logoURL: "/rimg/provider-logos/airlines/v/AT.png?crop=false&width=108&height=92&fallback=default3.png&_v=1737536ee712114d13825cc1be5ac586"
+    },
+
+    {
+    site: "https://www.avianca.com/otr/en",
+    code: "AV",
+    allliance: "SA",
+    phone: "+57 18000 953 434",
+    name: "Avianca",
+    defaultName: null,
+    __clazz: "com.r9.harmony.httpd.mobileapis.AirlineMobile",
+    __clazz: null,
+    logoURL: "/rimg/provider-logos/airlines/v/AV.png?crop=false&width=108&height=92&fallback=default2.png&_v=4b0e0000efbc54f535fc6b466987c6b0"
+    },
+
+    {
+    site: "https://www.finnair.com/int/gb/",
+    code: "AY",
+    allliance: "OW",
+    phone: "+358 600 140 140",
+    name: "Finnair",
+    defaultName: null,
+    __clazz: "com.r9.harmony.httpd.mobileapis.AirlineMobile",
+    __clazz: null,
+    logoURL: "/rimg/provider-logos/airlines/v/AY.png?crop=false&width=108&height=92&fallback=default2.png&_v=381727f16ff1a212dffe2239684e67ab"
+    },
+
+    {
+    site: "https://www.itaspa.com/en_en/",
+    code: "AZ",
+    allliance: "ST",
+    phone: "+39 06 65649",
+    name: "ITA - Airways",
+    defaultName: null,
+    __clazz: "com.r9.harmony.httpd.mobileapis.AirlineMobile",
+    __clazz: null,
+    logoURL: "/rimg/provider-logos/airlines/v/AZ.png?crop=false&width=108&height=92&fallback=default3.png&_v=adbd87d64b7ff99bbc7ea93edc5a41e3"
+    },
+
+    {
+    site: "https://www.iberia.com",
+    code: "IB",
+    allliance: "OW",
+    phone: "+34 901 111 500",
+    name: "Iberia",
+    defaultName: null,
+    __clazz: "com.r9.harmony.httpd.mobileapis.AirlineMobile",
+    __clazz: null,
+    logoURL: "/rimg/provider-logos/airlines/v/IB.png?crop=false&width=108&height=92&fallback=default2.png&_v=5b3e3a04002687362658ff8bb6803351"
+    },
+
+    {
+    site: "https://www.klm.com/home/us/en",
+    code: "KL",
+    allliance: "ST",
+    phone: "+55 11 38788363",
+    name: "KLM",
+    defaultName: null,
+    __clazz: "com.r9.harmony.httpd.mobileapis.AirlineMobile",
+    __clazz: null,
+    logoURL: "/rimg/provider-logos/airlines/v/KL.png?crop=false&width=108&height=92&fallback=default2.png&_v=c00721138846ce25f134390de9e21708"
+    },
+
+    {
+    site: "https://www.austrian.com",
+    code: "OS",
+    allliance: "SA",
+    phone: "+43 5 1766 3100",
+    name: "Austrian Airlines",
+    defaultName: null,
+    __clazz: "com.r9.harmony.httpd.mobileapis.AirlineMobile",
+    __clazz: null,
+    logoURL: "/rimg/provider-logos/airlines/v/OS.png?crop=false&width=108&height=92&fallback=default1.png&_v=2247889502335074ad57369cd6351053"
+    },
+
+    {
+    site: "https://www.singaporeair.com",
+    code: "SQ",
+    allliance: "SA",
+    phone: "+65 6223 8888",
+    name: "Singapore Airlines",
+    defaultName: null,
+    __clazz: "com.r9.harmony.httpd.mobileapis.AirlineMobile",
+    __clazz: null,
+    logoURL: "/rimg/provider-logos/airlines/v/SQ.png?crop=false&width=108&height=92&fallback=default3.png&_v=0ff33e55a8f21c7c3192f0394e8dfdb2"
+    },
+
+    {
+    site: "https://www.AA.com",
+    code: "AA",
+    allliance: "OW",
+    phone: "+1 800 433 7300",
+    name: "American Airlines",
+    defaultName: null,
+    __clazz: "com.r9.harmony.httpd.mobileapis.AirlineMobile",
+    __clazz: null,
+    logoURL: "/rimg/provider-logos/airlines/v/AA.png?crop=false&width=108&height=92&fallback=default2.png&_v=8c6d0e40bdbaad9c51e737b05c12347f"
+    },
+
+    {
+    site: "https://www.fijiairways.com",
+    code: "FJ",
+    allliance: "OW",
+    phone: "+679 672 0888",
+    name: "Fiji Airways",
+    defaultName: null,
+    __clazz: "com.r9.harmony.httpd.mobileapis.AirlineMobile",
+    __clazz: null,
+    logoURL: "/rimg/provider-logos/airlines/v/FJ.png?crop=false&width=108&height=92&fallback=default1.png&_v=9b38c3bf0c4447c0b6d5b340dc9c41b7"
+    },
+
+    {
+    site: "https://www.qantas.com/",
+    code: "QF",
+    allliance: "OW",
+    phone: "46 8 791 5922",
+    name: "Qantas Airways",
+    defaultName: null,
+    __clazz: "com.r9.harmony.httpd.mobileapis.AirlineMobile",
+    __clazz: null,
+    logoURL: "/rimg/provider-logos/airlines/v/QF.png?crop=false&width=108&height=92&fallback=default2.png&_v=96fac8e82c70a144957e62d820052184"
+    },
+
+    {
+    site: "https://www.flysaa.com/za/en/home.action",
+    code: "SA",
+    allliance: "SA",
+    phone: "+27 11 978 1111",
+    name: "South African",
+    defaultName: null,
+    __clazz: "com.r9.harmony.httpd.mobileapis.AirlineMobile",
+    __clazz: null,
+    logoURL: "/rimg/provider-logos/airlines/v/SA.png?crop=false&width=108&height=92&fallback=default2.png&_v=4f18918ac3fde0474c49c09d44a0e9bd"
+    },
+
+    {
+    site: "https://www.brusselsairlines.com",
+    code: "SN",
+    allliance: "SA",
+    phone: "+32 2 723 2362",
+    name: "Brussels Airlines",
+    defaultName: null,
+    __clazz: "com.r9.harmony.httpd.mobileapis.AirlineMobile",
+    __clazz: null,
+    logoURL: "/rimg/provider-logos/airlines/v/SN.png?crop=false&width=108&height=92&fallback=default3.png&_v=123f7f222ad9756b5898357021dbb757"
+    },
+
+    {
+    site: "https://www.airthanlwin.com",
+    code: "ST",
+    allliance: "none",
+    phone: "(+95-9) 766661010",
+    name: "Air Thanlwin",
+    defaultName: null,
+    __clazz: "com.r9.harmony.httpd.mobileapis.AirlineMobile",
+    __clazz: null,
+    logoURL: "/rimg/provider-logos/airlines/v/ST.png?crop=false&width=108&height=92&fallback=default3.png&_v=5a4d0a9351d6f6277f45d8ba5e25113a"
+    },
+    
+    {
+    site: "https://www.aireuropa.com",
+    code: "UX",
+    allliance: "ST",
+    phone: "+30 2238 5436",
+    name: "Air Europa",
+    defaultName: null,
+    __clazz: "com.r9.harmony.httpd.mobileapis.AirlineMobile",
+    __clazz: null,
+    logoURL: "/rimg/provider-logos/airlines/v/UX.png?crop=false&width=108&height=92&fallback=default3.png&_v=5ea611d69b085e12734d9ebece974657"
+    },
+];
+
